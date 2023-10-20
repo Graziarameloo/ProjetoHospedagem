ca1ba09f410a1769a2bdb65c108d10d749845640
Graziarameloo <graziarameloo@gmail.com>
2023-10-20 10:47:18 -0300

    Projeto hospedagem pronto

---
+++ ProjetoHospedagem/Models/Reserva.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+
+
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+
+          
+
+
+                if (hospedes.Count <= Suite.Capacidade)
+                {
+
+                    Hospedes = hospedes;
+                }
+                else
+                {
+
+                throw new Exception("A quantidade de hóspedes não pode exceder a capacidade da suíte");
+                
+
+            }
+
+           
+
+
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+
+        public int ObterQuantidadeHospedes()
+        {
+
+            int quantidade = Hospedes.Count;
+            return quantidade;
+
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+          
+
+
+
+            if (DiasReservados >= 10)
+            {
+               
+                valor = (DiasReservados * Suite.ValorDiaria) - (valor * 0.10M );
+
+            }
+
+            return valor;
+        }
+
+
+    }
+}
